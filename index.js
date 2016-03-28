d3e594d12877daddff08a5b00f5ebdd3ec29b4d0
denver <denver@projectcodex.co>
2016-03-28 12:45:24 +0200

    updated app

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var express = require('express');
+const express = require('express'),
+      app     = express();
 
-var app = express();
 app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.send('...');
 })
 
-app.listen(3001, function(){
-    console.log('app running');
+const port = process.env.PORT || 3001;
+const server = app.listen(port, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('App running on http://%s:%s', host, port);
 });
